e2744da7300c8036fb4b4a6f36fd00b61fcd082c
lex <nathan.richard.vg@gmail.com>
2026-04-26 22:32:58 +0200

    File 1 : Working but not enough

--- ray.c
+++ ray.c
@@ -6,6 +6,12 @@
 #define WIDTH 1200
 #define HEIGHT 900
 #define WHITE 0xffffff
+#define BLACK 0x000000
+#define RAY_COLOR 0xdbb407
+
+
+// ------------------------ STRUCTS -------------------------------
+
 
 typedef struct Circle
 {
@@ -13,6 +19,16 @@ typedef struct Circle
   double y;
   double radius;
 } Circle;
+
+
+typedef struct Ray 
+{
+  double ox, oy;
+  double dx, dy;
+} Ray;
+
+
+// ------------------------ METHODS -------------------------------
 
 Circle* initializeCircle(double x, double y, double radius)
 {
@@ -26,16 +42,35 @@ Circle* initializeCircle(double x, double y, double radius)
   return circle1;
 }
 
-void drawCircle(SDL_Surface* surface, Circle* circle)
+void drawCircle(SDL_Surface* surface, Circle* circle, int color)
 {
     for (int y = -circle->radius; y <= circle->radius; y++)
     {
         int x = (int)sqrt(circle->radius * circle->radius - y * y);
 
         SDL_Rect ligne = {circle->x - x, circle->y + y, 2*x, 1};
-        SDL_FillRect(surface, &ligne, WHITE);
+        SDL_FillRect(surface, &ligne, color);
     }
 }
+
+double intersectCircle(Ray* ray, Circle* circle)
+{
+    double fx = ray->ox - circle->x;
+    double fy = ray->oy - circle->y;
+
+    double a = ray->dx * ray->dx + ray->dy * ray->dy;
+    double b = 2 * (fx * ray->dx + fy * ray->dy);
+    double c = fx*fx + fy*fy - circle->radius * circle->radius;
+
+    double discriminant = b*b - 4*a*c;
+
+    if (discriminant < 0)
+        return -1;
+
+    return (-b - sqrt(discriminant)) / (2*a);
+}
+
+// ------------------------ MAIN -------------------------------
 
 int main(void)
 {
@@ -43,12 +78,64 @@ int main(void)
   SDL_Window* window = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
   SDL_Surface* surface = SDL_GetWindowSurface(window);
   
-  Circle* circleSol = initializeCircle(800, 430, 100);
-  drawCircle(surface, circleSol);
+  Circle* circlePlanet = initializeCircle(800, 430, 100);
+  Circle* circleSol = initializeCircle(300, 550, 50);
+  SDL_Rect eraser = {0, 0, WIDTH, HEIGHT};
 
-  Circle* circlePlanet = initializeCircle(300, 550, 50);
-  drawCircle(surface, circlePlanet);
+  int simulation_running = 1;
+  SDL_Event event;
+  while (simulation_running)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_QUIT)
+      {
+        simulation_running = 0;
+      }
+      if(event.type == SDL_MOUSEMOTION && event.motion.state != 0)
+      {
+        circleSol->x = event.motion.x;
+        circleSol->y = event.motion.y;
+      }
+    }
+    SDL_FillRect(surface, &eraser, BLACK);
 
-  SDL_UpdateWindowSurface(window);
-  SDL_Delay(3000);
+
+// ------------- RAY DRAWING ---------------
+
+    for (int py = 0; py < HEIGHT; py++)
+    {
+        for (int px = 0; px < WIDTH; px++)
+        {
+          Ray ray = {px, py, circleSol->x - px, circleSol->y - py};
+          double len = sqrt(ray.dx*ray.dx + ray.dy*ray.dy);
+          ray.dx /= len;
+          ray.dy /= len;
+
+          double t = intersectCircle(&ray, circlePlanet);
+
+          Uint32 color;
+          if (t > 0 && t < len)
+              color = BLACK;
+          else
+              color = RAY_COLOR;
+
+          SDL_Rect pixel = {px, py, 1, 1};
+          SDL_FillRect(surface, &pixel, color);
+        }
+    }
+  
+
+    drawCircle(surface, circleSol, WHITE);
+    drawCircle(surface, circlePlanet, WHITE);
+
+
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(10);
+
+  }
+
+  free(circlePlanet);
+  free(circleSol);
+
 }
